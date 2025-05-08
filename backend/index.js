d4dcc3a2d5cbb2512ffd06ffd09f459351ac4640
rupakkumar05 <rupakkumar995554@gmail.com>
2025-05-08 23:03:54 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // Load .env
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
